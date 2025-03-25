8aa4739c83609de1153281ef9f464dcbec3c8777
SaifShaikh00231 <saifshaikh00231@gmail.com>
2025-03-25 21:33:11 +0530

    first commit

---
+++ frontend/src/landing_page/home/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="Hero Image"
+          className="mb-5 w-100"
+        />
+<div className="col-12 d-flex justify-content-center">
+          <h1 className="mt-2">Invest in everything</h1>
+        </div>        
+        <div className="col-12 d-flex justify-content-center">
+          <p>
+            Online platform to invest in stocks, derivatives, mutual funds, and
+            more
+          </p>
+        </div>
+        <div className="col-12 d-flex justify-content-center">
+          <button
+            className="p-2 btn btn-primary fs-5 mb-5"
+            style={{ width: "20%", margin: "0 auto" }}
+          >
+            Signup Now
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
